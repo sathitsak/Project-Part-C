70029910b17eab75ca1a187efbe441062f5225f1
Connor Mclean <c.mclean8@student.unimelb.edu.au>
2018-05-22 16:44:10 +1000

    Created path and step objects for pathfinding

---
+++ Part C/src/mycontroller/Step.java
@@ -0,0 +1,21 @@
+package mycontroller;
+
+
+//Step object for Path class
+public class Step {
+
+	private int x, y;	
+	
+	public Step(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public int getX(){
+		return this.x;
+	}
+	
+	public int getY() {
+		return this.y;
+	}
+}
